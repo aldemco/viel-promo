7ee39d1f09b26b49aa4c64ee7f0045ba8780ae2d
Aldemco <krdluad@gmail.com>
2023-06-24 16:13:49 +0300

    add main site files

--- dist/index.php
+++ dist/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 
@@ -14,7 +13,7 @@
 	<link rel="icon" href="medical/images/logo.png">
 	<meta property="og:image" content="images/prof-illustration.png">
 	
-	<link rel="stylesheet" href="medical/css/app.min.css?v=1.9">
+	<link rel="stylesheet" href="medical/css/app.min.css?v={BUILD_ID}">
 
 </head>
 
@@ -970,25 +969,8 @@
 </div>
 
 
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();
-    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
-    ym(93859979, "init", {
-         clickmap:true,
-         trackLinks:true,
-         accurateTrackBounce:true,
-         webvisor:true
-    });
- </script>
- <noscript><div><img src="https://mc.yandex.ru/watch/93859979" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
- <!-- /Yandex.Metrika counter -->
 
-	<script src="js/app.min.js?v=1.9"></script>
+<script src="medical/js/app.min.js?v={BUILD_ID}"></script>
 
 </body>
 </html>
