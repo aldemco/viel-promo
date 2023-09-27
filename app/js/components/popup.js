64ce8595a4d35e77495925e1655241dfeb346b92
Aldemco <krdluad@gmail.com>
2023-09-27 17:19:53 +0300

    add vue

--- app/js/components/popup.js
+++ app/js/components/popup.js
@@ -19,7 +19,12 @@ export default {
     <div v-if="isOpen" @click="close" class="popup-background"></div>
     <div v-if="isOpen" v-bind="$attrs" class="popup">
         <div class="popup__top-bar">
-            <a class="popup__close" href="#" @click="close"> <img src="images/close.svg" alt="close"></a>
+            <a class="popup__close" href="#" @click="close"> 
+              <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M31 2L2 31" stroke="#7CA950" stroke-width="3" stroke-linecap="round"/>
+                <path d="M2 2L31 31" stroke="#7CA950" stroke-width="3" stroke-linecap="round"/>
+              </svg>
+            </a>
         </div>
         <div class="popup__body">
              <slot></slot> 
