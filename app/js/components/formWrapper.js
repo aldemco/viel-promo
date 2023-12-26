e0860a8f5ab0df5899efbd548e404b06ff44578f
Aldemco <krdluad@gmail.com>
2023-12-26 17:28:28 +0300

    update forms

--- app/js/components/formWrapper.js
+++ app/js/components/formWrapper.js
@@ -30,6 +30,7 @@ export default {
     methods: {
       submit(event) {
         this.isLoading = true
+        this.form.secret = new Date().toISOString();
         axios({
             method: this.method,
             url: this.endpoint,
